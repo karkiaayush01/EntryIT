30ae5ef7e139aa0fde976a242a1e6139c790c60b
Aayush Karki <karkiaayush01@gmail.com>
2025-12-20 09:23:22 +0545

    Feat: Initial Commit

---
+++ EntryIt/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EntryIt
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
